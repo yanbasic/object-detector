0118bb0a15238cf5554717ca7f83259197cf6b0a
Yi Yan <yiyanz@amazon.com>
2025-02-17 13:54:48 +0800

    add comments

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import cv2
 import torchvision
 import time
 
+# add comments
 
 def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
